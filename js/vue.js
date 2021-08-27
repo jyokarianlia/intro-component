23d43404e292b909707744a5a3c462b87138e8df
jyo-linux <jyokari51@gmail.com>
2021-08-27 11:36:11 -0500

    Agregado de estilos e icono de error de insercion de datos primera capa

--- js/vue.js
+++ js/vue.js
@@ -19,21 +19,45 @@ const app = new Vue({
             if (this.firstName == '') {
                 this.msjErrorNane = true;
                 this.classErrorName = 'inputError';
+            } else {
+                this.msjErrorNane = false;
+                this.classErrorName = '';
             }
 
             if (this.lastName == '') {
                 this.msjErrorLastName = true; 
-                this.classErrorLastName = 'inputError';
+                if (this.msjErrorNane) {
+                    this.classErrorLastName = 'inputError marginError';
+                } else {
+                    this.classErrorLastName = 'inputError marginError2';
+                }
+            } else {
+                this.msjErrorLastName = false; 
+                this.classErrorLastName = '';
             }
 
             if (this.email == '') { 
                 this.msjErrorEmail = true; 
-                this.classErrorEmail = 'inputError';
+                if (this.msjErrorLastName) {
+                    this.classErrorEmail = 'inputError marginError';
+                } else {
+                    this.classErrorEmail = 'inputError marginError2';
+                }
+            } else {
+                this.msjErrorEmail = false; 
+                this.classErrorEmail = '';
             }
 
             if (this.password == '') { 
                 this.msjErrorPassword = true; 
-                this.classErrorPassword = 'inputError';
+                if (this.msjErrorEmail) {
+                    this.classErrorPassword = 'inputError marginError';
+                } else {
+                    this.classErrorPassword = 'inputError marginError2';
+                }
+            } else {
+                this.msjErrorPassword = false; 
+                this.classErrorPassword = '';
             }
             console.log('Si esta funcionando el evento');
         }
